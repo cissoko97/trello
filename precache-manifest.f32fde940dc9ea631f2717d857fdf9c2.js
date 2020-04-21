f2b5f92fba2e2b6cc62057f8737f12af9f34c78f
cissoko97 <boriscissoko@gmail.com>
2020-04-21 19:07:17 +0100

    Updates

---
+++ precache-manifest.f32fde940dc9ea631f2717d857fdf9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4dbdc5fc3857866773cec2f05210b3",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "25f6012be5c93698c695",
+    "url": "/trello/static/css/main.ad9fc93f.chunk.css"
+  },
+  {
+    "revision": "ba17203b4be4a26444b5",
+    "url": "/trello/static/js/2.204d2f07.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/trello/static/js/2.204d2f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f6012be5c93698c695",
+    "url": "/trello/static/js/main.e510c3c3.chunk.js"
+  },
+  {
+    "revision": "494656f04a239a80315c",
+    "url": "/trello/static/js/runtime-main.59ac0cc2.js"
+  }
+]);
